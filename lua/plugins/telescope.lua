0f689f3eab9c7d2fa97bc3b1297e3afd4ac27503
rammah karpous <rammahkarpous@outlook.com>
2024-03-03 03:09:16 +0100

    ignored some folders in the telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,14 @@ return {
     'nvim-telescope/telescope.nvim', 
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    
     config = function()
+        require('telescope').setup({
+            defaults = {
+                file_ignore_patterns = { "node_modules", "vendor", "stubs", "storage" }
+            }
+        })
+
         local builtin = require("telescope.builtin")
 
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
